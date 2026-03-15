06267d63303c5a61c22dee2abfbf7816c9286785
sarmadna <sarmad_n_a@hotmail.com>
2026-03-15 13:02:29 +0300

    update

--- flaskemr/__init__.py
+++ flaskemr/__init__.py
@@ -1,16 +1,14 @@
-import os
+#import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from sqlalchemy.sql import func
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+#basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "fjasdfj309wef093qkjeflj03qjqdkfjksaf"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'clinic.db')
+#app.config['SECRET_KEY'] = "fjasdfj309wef093qkjeflj03qjqdkfjksaf"
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'clinic.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clinic.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 from flaskemr import routes
