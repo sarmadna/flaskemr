759435fa16347ecca8c432a2e2439a5e3131c75f
sarmadna <sarmad_n_a@hotmail.com>
2026-03-19 09:29:32 +0300

    update

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flaskemr import app, db
-#from waitress import serve
+from waitress import serve
 
 if __name__ == "__main__":
-    app.run(debug=True)
-#    with app.app_context():
-#        db.create_all()
-#    serve(app, host="0.0.0.0", port=5000)
+    with app.app_context():
+        db.create_all()
+    serve(app, host="0.0.0.0", port=5000)
