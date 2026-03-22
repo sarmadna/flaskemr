4670d3f457f23ea6b1b1a4459e65230db921aa4e
sarmadna <sarmad_n_a@hotmail.com>
2026-03-22 15:15:37 +0300

    update

--- flaskemr/models.py
+++ flaskemr/models.py
@@ -10,10 +10,10 @@ class Client(db.Model):
     mnm = db.Column(db.String(20), nullable=False)
     lnm = db.Column(db.String(20), nullable=False)
     sex = db.Column(db.String(20), nullable=False)
-    dob = db.Column(db.Integer, nullable=False)
+    dob = db.Column(db.Date, nullable=False)
     adr = db.Column(db.String(20), nullable=False)
     tel = db.Column(db.String(20), nullable=False)
-    visits = db.relationship("Visit", backref="clients", cascade="all, delete")
+    visits = db.relationship("Visit", backref="client", cascade="all, delete")
 
     def __init__(self, fnm, mnm, lnm, sex, dob, adr, tel):
         self.fnm = fnm
@@ -23,6 +23,10 @@ class Client(db.Model):
         self.dob = dob
         self.adr = adr
         self.tel = tel
+
+    @property
+    def display_pid(self):
+        return f"CL-{self.pid:04d}"
 
     def __repr__(self):
         return f"Client({self.fnm}, {self.mnm}, {self.lnm}, {self.sex}, {self.dob}, {self.adr}, {self.tel})"
@@ -34,28 +38,29 @@ class Visit(db.Model):
 
     vid = db.Column(db.Integer, primary_key=True)
     cid = db.Column(db.Integer, db.ForeignKey("clients.pid"), nullable=False)
-    dov = db.Column(db.Integer, nullable=False)
-    mov = db.Column(db.Integer, nullable=False)
-    yov = db.Column(db.Integer, nullable=False)
-    cc = db.Column(db.String(90), nullable=False)
-    dx = db.Column(db.String(90), nullable=False)
+    dat = db.Column(db.Date, nullable=False)
+    ccc = db.Column(db.String(90), nullable=False)
+    dxx = db.Column(db.String(90), nullable=False)
     rx1 = db.Column(db.String(90), nullable=False)
     rx2 = db.Column(db.String(90), nullable=False)
     rx3 = db.Column(db.String(90), nullable=False)
     rx4 = db.Column(db.String(90), nullable=False)
 
-    def __init__(self, cid, dov, mov, yov, cc, dx, rx1, rx2, rx3, rx4):
-        self.dov = dov
-        self.mov = mov
-        self.yov = yov
-        self.cc = cc
-        self.dx = dx
+    def __init__(self, cid, dat, ccc, dxx, rx1, rx2, rx3, rx4):
+        self.dat = dat
+        self.ccc = ccc
+        self.dxx = dxx
         self.rx1 = rx1
         self.rx2 = rx2
         self.rx3 = rx3
         self.rx4 = rx4
         self.cid = cid
 
+    @property
+    def display_vid(self):
+        year = self.dat.year
+        return f"VT-{year}-{self.vid:04d}"
+
     def __repr__(self):
-        return f"Visit({self.cid}, {self.dov}, {self.mov}, {self.yov}, {self.cc}, {self.dx}, {self.rx1}, {self.rx2}, {self.rx3}, {self.rx4})"
+        return f"Visit({self.cid}, {self.dat}, {self.ccc}, {self.dxx}, {self.rx1}, {self.rx2}, {self.rx3}, {self.rx4})"
     
